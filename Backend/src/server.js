5bf81e357f0e8eab6b252faa79e3280cae3a7022
hishmaiyoob <hishmahish13@gmail.com>
2026-09-15 14:52:26 +0530

    fixed production cors

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -7,18 +7,30 @@ import authRoutes from "./routes/authRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
 import dashboardRoutes from "./routes/dashboardRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-import adminRoutes from "./routes/adminRoutes.js"
+import adminRoutes from "./routes/adminRoutes.js";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
 
 // Connect to database
 connectDB();
 
+// allowed frontend origins
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://taskly-five-peach.vercel.app",
+];
+
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 
 // Routes
@@ -26,7 +38,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/users", userRoutes);
-app.use("/api/admin", adminRoutes)
+app.use("/api/admin", adminRoutes);
 
 // Health check
 app.get("/", (req, res) => {
